b6a786ad1460136cbc0bee94cd41544421c7d476
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-12-05 18:42:54 +0530

    ed

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,34 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
-  const [timer,setTimer]=useState(0);
-  const handleKeydown=(e)=>{
-    if(e.keyCode!==13){return;}
-     setTimer(e.target.value);
-  }
-  useEffect(()=>{
-    const id=setInterval(()=>{timer>=0?setTimer(timer-1):null;},1000);
-    return ()=>clearInterval(id);
-  },[timer]);
+  // write your code here
+  const [timer, setTimer] = useState(0);
+  const handleKeydown = (e) => {
+    if (e.keyCode !== 13) {
+      return;
+    }
+    if (isNaN(e.target.value) || parseInt(e.target.value) < 0) return;
+    setTimer(e.target.value);
+  };
+  useEffect(() => {
+    const id = setInterval(() => {
+      timer > 0 ? setTimer(timer - 1) : null;
+    }, 1000);
+    return () => clearInterval(id);
+  }, [timer]);
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={handleKeydown} /> sec.
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={handleKeydown} /> sec.
         </h1>
       </div>
       <div id="current-time">{timer}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
