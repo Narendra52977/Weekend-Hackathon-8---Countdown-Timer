733d9e765ef44d9fd9e94c56900846ec7bd18318
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-12-05 18:38:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,24 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [timer,setTimer]=useState(0);
+  const handleKeydown=(e)=>{
+    if(e.keyCode!==13){return;}
+     setTimer(e.target.value);
+  }
+  useEffect(()=>{
+    const id=setInterval(()=>{timer>=0?setTimer(timer-1):null;},1000);
+    return ()=>clearInterval(id);
+  },[timer]);
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={handleKeydown} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timer}</div>
     </div>
   )
 }
