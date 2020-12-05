e792b029e022c97b30c8dfb996de119c20ab6056
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-12-05 19:01:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,9 +14,44 @@ const App = () => {
     } else setTimer(0);
   };
   useEffect(() => {
-    const id = setInterval(() => {
-      timer > 0 ? setTimer(timer - 1) : null;
-    }, 1000);
+    import React, { Component, useState, useEffect } from "react";
+import "../styles/App.css";
+
+const App = () => {
+  const [time, setTime] = useState(0);
+
+  const handleTime = (event) => {
+    if (event.keyCode !== 13) {
+      return;
+    }
+
+    let timeCount = Math.floor(event.target.value);
+    if (typeof timeCount === "number" && timeCount >= 0) {
+      setTime(timeCount);
+    } else setTime(0);
+  };
+  useEffect(() => {
+    const id =
+      time > 0 &&
+      setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+    return () => clearInterval(id);
+  }, [time]);
+  return (
+    <div className="wrapper">
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={(event) => handleTime(event)} /> sec.
+        </h1>
+      </div>
+      <div id="current-time">{time}</div>
+    </div>
+  );
+};
+
+export default App;
     return () => clearInterval(id);
   }, [timer]);
 
